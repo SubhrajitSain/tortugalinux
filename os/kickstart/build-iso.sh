86505e2ddfa3183ccd52f4b94bb9585864ad6956
ingStudiosOfficial <ingstudiosofficial@gmail.com>
2026-06-15 16:41:51 +0800

    fix: modify build-iso.sh

--- os/kickstart/build-iso.sh
+++ os/kickstart/build-iso.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 sudo setenforce 0
+
 mkdir -p build
-ksflatten -c ./turtlinux.ks -o ./build/flat-turtlinux.ks
+
+# Configure your Kickstart file here
+kickstart_file="turtlinux-test.ks"
+
+ksflatten -c ./${kickstart_file} -o ./build/flat-${kickstart_file}
+
 cd build
+
 rm -rf ./iso_out
+
 sudo livemedia-creator \
     --make-iso \
-    --ks=./flat-turtlinux.ks \
+    --ks=./flat-${kickstart_file} \
     --no-virt \
     --resultdir=./iso_out \
     --project=TurtLinux \
     --releasever=44 \
     --iso-only \
     --iso-name=turtlinux-live.iso
-sudo setenforce
+
+sudo setenforce 1
